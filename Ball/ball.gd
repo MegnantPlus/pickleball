70b991fb638eab404193eddeccab67cff2759d1f
MoriFukasaki <0368269@st.huce.edu.vn>
2025-11-05 13:45:10 +0700

    pickleball update v1

--- Ball/ball.gd
+++ Ball/ball.gd
@@ -6,7 +6,8 @@ var speed = 300
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	gravity_scale = 0
-	
+	linear_damp = 0
+	angular_damp = 0
 	var direction = Vector2(randf_range(-1,1),randf_range(-1,1)).normalized()
 	linear_velocity = direction * speed
 
@@ -16,5 +17,6 @@ func _process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node) -> void:
-	if body.name == "Pan":
+	if (body.name == "Pan" or body.name == "Ground" or body.name == "Floor" or body.name == "LeftWall" or body.name == "RightWall"):
 		linear_velocity.x = - linear_velocity.x
+		linear_velocity = linear_velocity.normalized() * speed
